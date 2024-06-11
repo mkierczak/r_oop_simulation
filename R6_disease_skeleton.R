338c24973b3b300c2cece6d1edaeb5390658e859
mkierczak <kiero@kiero.pl>
2024-06-11 14:05:11 +0200

    add some comments

--- R6_disease_skeleton.R
+++ R6_disease_skeleton.R
@@ -2,17 +2,22 @@ library(R6)
 
 Agent <- R6Class("Agent",
                  public = list(
+                   # Data
                    state = character(),
-                   initialize = ,
-                   update_state = 
+                   
+                   # Methods
+                   initialize = function(),
+                   update_state = function()
                  )
 )
 
 World <- R6Class("World",
                  public = list(
+                   # Data
                    size = NULL,
                    world = NULL,
                    
+                   # Methods
                    initialize = function(size),
                    
                    add_agent = function(row, col, agent),
@@ -28,9 +33,14 @@ World <- R6Class("World",
 library(ggplot2)
 library(gganimate)
 
-run_simulation <- function(size, num_steps, initial_immune, initial_sick, beta, recovery_prob, death_prob) {
-  world <- World$new(size)
-  
+run_simulation <- function(size, 
+                           num_steps, 
+                           initial_immune, 
+                           initial_sick, 
+                           beta, 
+                           recovery_prob, 
+                           death_prob) {
+
   # Initialize agents
 
   # Introduce immune and sick agents
@@ -41,6 +51,7 @@ run_simulation <- function(size, num_steps, initial_immune, initial_sick, beta, 
   
   # Get counts and store them as a result
 
+}
 
 # Parameters
 size <- 20                    # World size
@@ -52,20 +63,3 @@ beta <- 0.5                  # Transmission probability
 recovery_prob <- 0.1
 death_prob <- 0.05
 
-# Run the simulation
-results <- run_simulation(size, num_steps, initial_immune, initial_sick, beta, recovery_prob, death_prob)
-
-library(tidyverse)
-results |> group_by(step) |> 
-  summarise(n_sick = sum(sick), n_healthy = sum(healthy), n_immune = sum(immune), n_dead = sum(dead)) |> 
-  pivot_longer(starts_with("n_"), names_to = 'measure') |> 
-  ggplot(aes(x = step, y = value, col=measure)) + geom_line() + theme_minimal()
-
-# Visualize the results
-visualize_simulation(results)
-
-
-#results %>% filter(step == 1) %>% ggplot(aes(x = col, y = row)) +
-geom_tile(aes(fill = sick)) +
-  scale_fill_gradient(low = "white", high = "red") +
-  theme_minimal()
